4d8e8c7f00e67b1c22df5b7b78c1743e4b692165
ibidyouadu <angel.d.umana@gmail.com>
2024-07-21 00:24:36 -0500

    Update views and templates to accept and render image input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, UploadFile, File, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from tempfile import NamedTemporaryFile
+import os
+import base64
 
 app = FastAPI()
 templates = Jinja2Templates("templates")
@@ -12,12 +15,13 @@ def index(request: Request):
     
     return response
 
-@app.post("/transforminate")
-def transforminate(request: Request, favnum: float = Form(...)):
-    output = favnum/3.14159
-    context = {"request": request, "output": output}
+@app.post("/result")
+def show_image(request: Request, background_tasks: BackgroundTasks, input_image: UploadFile = File(...)):
+    contents = input_image.file.read()
+    encoded_image = base64.b64encode(contents).decode("utf-8")
+    
+    context = {"request": request, "image": encoded_image}
     response = templates.TemplateResponse("result.html", context)
-
     return response
 
 if __name__ == "__main__":
